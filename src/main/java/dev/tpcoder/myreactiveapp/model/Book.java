1996beb73c7ef393f757e5407cd98183a04f5081
Thanaphoom Babparn <tp.babparn@gmail.com>
2021-01-27 20:54:26 +0700

    Commit 1: add book model

---
+++ src/main/java/dev/tpcoder/myreactiveapp/model/Book.java
@@ -0,0 +1,11 @@
+package dev.tpcoder.myreactiveapp.model;
+
+import org.springframework.data.relational.core.mapping.Table;
+
+import lombok.Data;
+
+@Data
+@Table("book")
+public class Book {
+  
+}
