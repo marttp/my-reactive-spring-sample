ab71e5510d5485413d3db88ebf76b8e9cb7efdc4
Thanaphoom Babparn <tp.babparn@gmail.com>
2021-01-27 20:42:05 +0700

    Initial project

---
+++ src/main/java/dev/tpcoder/myreactiveapp/MyReactiveAppApplication.java
@@ -0,0 +1,13 @@
+package dev.tpcoder.myreactiveapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyReactiveAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyReactiveAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/tpcoder/myreactiveapp/MyReactiveAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tpcoder.myreactiveapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyReactiveAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
