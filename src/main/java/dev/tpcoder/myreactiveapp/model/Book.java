d1f1f520d69c663649e301ff233ccd9e5eb53907
Thanaphoom Babparn <tp.babparn@gmail.com>
2021-01-27 21:02:02 +0700

    Book: Add more fields

--- src/main/java/dev/tpcoder/myreactiveapp/model/Book.java
+++ src/main/java/dev/tpcoder/myreactiveapp/model/Book.java
@@ -1,11 +1,19 @@
 package dev.tpcoder.myreactiveapp.model;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Table("book")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Book {
-  
+  @Id
+  private Long id;
+  private String title;
+  private String isbn;
 }
