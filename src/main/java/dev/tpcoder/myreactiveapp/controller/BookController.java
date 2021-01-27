2aac9767c72b30b3f5784fe565fd40aade9c48c0
Thanaphoom Babparn <tp.babparn@gmail.com>
2021-01-27 21:10:37 +0700

    Main: Add flux get all books

--- src/main/java/dev/tpcoder/myreactiveapp/controller/BookController.java
+++ src/main/java/dev/tpcoder/myreactiveapp/controller/BookController.java
@@ -1,8 +1,17 @@
 package dev.tpcoder.myreactiveapp.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import reactor.core.publisher.Flux;
+
 @RestController
+@RequestMapping("books")
 public class BookController {
   
+  @GetMapping
+  public Flux<String> getAllBooks() {
+    return Flux.just("Test");
+  }
 }
